5c82d79cb769f0f1061f26169601c2523df5b272
thegbars <gbarsotti262@gmail.com>
2026-02-27 16:19:44 -0500

    updated test content

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './Home.tsx'
+import '@/index.css'
+import Home from '@/Home.tsx'
 import { HashRouter, Routes, Route } from "react-router";
 
 
