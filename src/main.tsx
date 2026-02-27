417093b2c8a432ea2a86c3f802bc267146e61643
thegbars <gbarsotti262@gmail.com>
2026-02-27 16:06:09 -0500

    Vite, Tailwind, React Router, @ path

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { HashRouter, Routes, Route } from "react-router";
+
+
+createRoot(document.getElementById('root')!).render(
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </HashRouter>,
+)
